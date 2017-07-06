f9423a5c3ec7813d21e932cd2c7cc93ec3633ac0
Rich Temple <rich.otaku@gmail.com>
2017-07-06 14:08:10 -0400

    [EDU-7970] - Setup/config for new package.

--- setup.py
+++ setup.py
@@ -1,53 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import re
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 
-def get_version(*file_paths):
-    """Retrieves the version from django_admin_env_notice/__init__.py"""
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
-
-
-version = get_version("django_admin_env_notice", "__init__.py")
-
-
-if sys.argv[-1] == 'publish':
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
     try:
-        import wheel
-        print("Wheel version: ", wheel.__version__)
-    except ImportError:
-        print('Wheel library missing. Please run "pip install wheel"')
-        sys.exit()
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
-    sys.exit()
+        return open(filepath).read()
+    except IOError:
+        return ''
 
-if sys.argv[-1] == 'tag':
-    print("Tagging the version on git:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
-    sys.exit()
 
-readme = open('README.rst').read()
+def get_readme():
+    """Return the README file contents. Supports text,rst, and markdown"""
+    for name in ('README', 'README.rst', 'README.md'):
+        if os.path.exists(name):
+            return read_file(name)
+    return ''
+
+__import__('django_admin_env_notice')
+
+
+version = sys.modules['django_admin_env_notice'].get_version().replace(' ', '-')
+
 
 setup(
     name='django-admin-env-notice',
     version=version,
     description="""Visually distinguish environments in Django Admin""",
-    long_description=readme,
+    long_description=get_readme(),
     author='Iurii Shikanov',
     author_email='dizballanze@gmail.com',
     url='https://github.com/dizballanze/django-admin-env-notice',
